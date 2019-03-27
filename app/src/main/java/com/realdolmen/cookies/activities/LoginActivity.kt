f0125b30a9a1d8bc1f11d5e44bd5b82ac4785b59
TomVanDeSteene <tom.vandesteene@realdolmen.com>
2019-03-27 09:27:12 +0100

    made some layout changes

--- app/src/main/java/com/realdolmen/cookies/activities/LoginActivity.kt
+++ app/src/main/java/com/realdolmen/cookies/activities/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.realdolmen.cookies.activities
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -12,6 +13,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val mp = MediaPlayer.create (this, R.raw.something_special)
+        mp.start ()
 
         btn_login.setOnClickListener {
             val toast = Toast.makeText(this, "Login was pressed", Toast.LENGTH_SHORT)
