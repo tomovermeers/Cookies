13f7dd57fc65b1d9315cef24c743cb6e8d34f53c
TomVanDeSteene <tom.vandesteene@realdolmen.com>
2019-03-28 10:48:55 +0100

    Added SignupActivity

---
+++ app/src/main/java/com/realdolmen/cookies/activities/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.realdolmen.cookies.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.realdolmen.cookies.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
